d074395513baa2cec8b7cdfc0f2baf8c548e017c
Arun-kumar00 <arungoyal706@gmail.com>
2026-04-29 16:59:14 +0530

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,7 @@
-export default nextConfig;
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-        ],
-      },
-    ];
-  },
+  /* config options here */
+  reactCompiler: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
